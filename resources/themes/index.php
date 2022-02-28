febf0f292e38bd18cc5f08a7c0be1379da47debb
Byrdie <seabirdshanty@users.noreply.github.com>
2022-02-28 08:07:45 -0800

    Delete index.php

--- resources/themes/index.php
+++
@@ -1,9 +0,0 @@
-
-    <?php echo $gallery->getColorboxStyles(1); ?>
-    <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/style.css" />
-    <?php /* <script type="text/javascript" src="//code.jquery.com/jquery-2.1.4.min.js"></script> */ ?>
-    <?php // echo $gallery->getColorboxScripts(); ?>
-    <?php
-        $galleryArray['relText'] = 'colorbox';
-        echo $gallery->readTemplate('templates/defaultGallery.php', $galleryArray);
-    ?>
